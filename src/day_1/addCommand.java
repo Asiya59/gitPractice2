ea9acc9740102e12e21913f3c1214d895bea51b9
Asiya Eli <asiyae@outlook.com>
2020-04-28 15:26:20 -0400

    add addCommand.java

--- src/day_1/addCommand.java
+++ src/day_1/addCommand.java
@@ -1,17 +1,10 @@
 package day_1;
 
-public class Practice {
+public class addCommand {
 
     public static void main(String[] args) {
 
 
-
-
-
-
-
-
+        //I want to try git add filename
     }
 }
-
-
